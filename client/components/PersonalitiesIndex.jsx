78773d8ae48f49e4b102bb62727f5e5747afa4a9
Devon McGrath <devon.mcgrath@gmail.com>
2016-09-02 11:21:37 +1200

    divs and classes added and named. TODO: rename divs to more appropriate/less confusing

--- client/components/PersonalitiesIndex.jsx
+++ client/components/PersonalitiesIndex.jsx
@@ -7,15 +7,16 @@ export default (props) => {
   return (
     <div className="PersonalitiesIndex-container">
       <SubNav />
-      <h2>PersonalitiesIndex component</h2>
-      <div className="Personalities">
-        {/* these will be generated from the database */}
-        <div className="Personality-card">
-          <h3>Personality-card</h3>
-          <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+      <div className="Personalities-list-container">
+        <h2>PersonalitiesIndex component</h2>
+        <div className="Personalities-list">
+          {/* these will be generated from the database */}
+          <div className="Personality-card">
+            <h3>Personality-card</h3>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+          </div>
         </div>
       </div>
-
     </div>
   )
 }
