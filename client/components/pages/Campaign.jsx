1a2b1b55a518d66d5210acc824bd028f916782f4
Devon McGrath <devon.mcgrath@gmail.com>
2016-09-15 14:59:46 +1200

    campaign page update

--- client/components/pages/Campaign.jsx
+++ client/components/pages/Campaign.jsx
@@ -11,23 +11,24 @@ export default (props) => {
         <h1>Campaign Name Details</h1>
         <div className="Campaign-info">
           <h2>Campaign</h2>
-          <input type="text" name="Campaign-Name" value="Campaign Name"/>
+          <input type="text" name="Campaign-Name" placeholder="Campaign Name"/>
           <h2>Status</h2>
-          <input type="text" name="Campaign-Status" value="Select Campaign Status"/>
+          <input type="text" name="Campaign-Status" placeholder="Select Campaign Status"/>
           <h2>Personality</h2>
-          <input type="text" name="Campaign-Personality" value="Select Personality"/>
+          <input type="text" name="Campaign-Personality" placeholder="Select Personality"/>
 
           <div>
           <h2>Videos</h2>
           <button>Browse</button>
           <h2>Order</h2>
-          <button >Add</button>
+          <button>Add</button>
             <div>
               <PersonalityCard />
               <PersonalityCard />
               <PersonalityCard />
               <PersonalityCard />
             </div>
+          <button>SAVE/UPDATE</button>
           </div>
 
         </div>
